3441b548d96be2e43baac09d75224dbb53c21791
Renan <renanmagalhaesreis@gmail.com>
2024-09-13 19:45:22 -0300

    adicionando os apps na pagina de adminatracao do django

--- ingrediente/admin.py
+++ ingrediente/admin.py
@@ -1,1 +1,8 @@
 from django.contrib import admin
+from .models import Ingrediente
+
+@admin.register(Ingrediente)
+class IngredienteAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'tipo', 'preco_adicional')  
+    search_fields = ['nome'] 
+    list_filter = ['tipo']  
--- usuario/admin.py
+++ usuario/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Usuario
 
-# Register your models here.
+@admin.register(Usuario)
+class UsuarioAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'cpf', 'telefone', 'endereco')  # Colunas que aparecerão no admin
+    search_fields = ['nome', 'cpf']  # Campos de busca
