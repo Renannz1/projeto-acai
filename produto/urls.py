5e899b0a754dfcf1b683d7bd6da9e23904e8f708
Renan <renanmagalhaesreis@gmail.com>
2024-10-28 19:32:36 -0300

    adicionado funcao ProdutoCreateView e ProdutoUpdateView

--- produto/urls.py
+++ produto/urls.py
@@ -3,9 +3,12 @@ from . import views
 
 urlpatterns = [
 #   path('', views.listar_produtos, name='listar_produtos'),
-    path('adicionar/', views.adicionar_produto, name='adicionar_produto'),
-    path('editar/<int:produto_id>/', views.editar_produto, name='editar_produto'),
+#   path('adicionar/', views.adicionar_produto, name='adicionar_produto'),
+#   path('editar/<int:produto_id>/', views.editar_produto, name='editar_produto'),
     path('remover/<int:produto_id>/', views.remover_produto, name='remover_produto'),
 
-    path('listar/', views.ProdutoListView.as_view(), name= 'listar_produtos'),
+    path('', views.ProdutoListView.as_view(), name= 'listar_produtos'),
+    path('adicionar/', views.ProdutoCreateView.as_view(), name= 'adicionar_produto'),
+    path('editar/<int:pk>', views.ProdtuoUpdateView.as_view(), name= 'editar_produto'),
+
 ]
--- produto/views.py
+++ produto/views.py
@@ -1,4 +1,6 @@
+from django import forms
 from django.shortcuts import render, get_object_or_404, redirect
+from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, UpdateView, CreateView, DeleteView
 from .models import Produto
 from .forms import ProdutoForm
@@ -7,26 +9,26 @@ from .forms import ProdutoForm
 #     produtos = Produto.objects.all()
 #     return render(request, 'produto/listar_produtos.html', {'produtos': produtos})
 
-def adicionar_produto(request):
-    if request.method == 'POST':
-        form = ProdutoForm(request.POST, request.FILES)
-        if form.is_valid():
-            form.save()
-            return redirect('listar_produtos')
-    else:
-        form = ProdutoForm()
-    return render(request, 'produto/adicionar_produto.html', {'form': form})
+# def adicionar_produto(request):
+#     if request.method == 'POST':
+#         form = ProdutoForm(request.POST, request.FILES)
+#         if form.is_valid():
+#             form.save()
+#             return redirect('listar_produtos')
+#     else:
+#         form = ProdutoForm()
+#     return render(request, 'produto/adicionar_produto.html', {'form': form})
 
-def editar_produto(request, produto_id):
-    produto = get_object_or_404(Produto, id=produto_id)
-    if request.method == 'POST':
-        form = ProdutoForm(request.POST, request.FILES, instance=produto)
-        if form.is_valid():
-            form.save()
-            return redirect('listar_produtos')
-    else:
-        form = ProdutoForm(instance=produto)
-    return render(request, 'produto/editar_produto.html', {'form': form})
+# def editar_produto(request, produto_id):
+#     produto = get_object_or_404(Produto, id=produto_id)
+#     if request.method == 'POST':
+#         form = ProdutoForm(request.POST, request.FILES, instance=produto)
+#         if form.is_valid():
+#             form.save()
+#             return redirect('listar_produtos')
+#     else:
+#         form = ProdutoForm(instance=produto)
+#     return render(request, 'produto/editar_produto.html', {'form': form})
 
 def remover_produto(request, produto_id):
     produto = get_object_or_404(Produto, id=produto_id)
@@ -37,8 +39,26 @@ def remover_produto(request, produto_id):
 
 
 
-## USANDO CLASSES
+## USANDO CLASSES GENERICAS DO DJANGO
+
+
+
+class ProdutoCreateView(CreateView):
+    model = Produto
+    template_name = "produto/adicionar_produto.html"
+    fields = '__all__'
+    success_url = 'listar_produtos'
+
 class ProdutoListView(ListView):
     model = Produto
     template_name = "produto/listar_produtos.html"
     context_object_name = 'produtos'
+
+class ProdtuoUpdateView(UpdateView):
+    model = Produto
+    template_name = "produto/editar_produto.html"
+    fields = ['nome', 'preco', 'sabor', 'acompanhamentos', 'imagem']
+    success_url = reverse_lazy('listar_produtos')
+
+
+
